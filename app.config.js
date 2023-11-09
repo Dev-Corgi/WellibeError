aff580e0861c8221fe5a7f65d82ceb4344be81e6
Dev-Corgi <bora5242@gmail.com>
2023-11-10 08:30:36 +0900

    test3

--- app.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ config }) => ({
-    name: "Wellibe",
-    slug: "wellibe",
-    extra: {
-      eas: {
-        projectId: "3a41539f-eae6-44f8-a8d5-c9622ee3b61a"
-      }
-    },
-  });
